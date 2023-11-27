09417a8c41663b7f20ce2e946062b8b59ce9f3fe
Amalia Sakinah Putri <amalia2019@gmail.com>
2023-11-27 14:59:09 +0700

    update video5

--- AmaliaSakinahPutri/video4/my-app/src/index.js
+++ AmaliaSakinahPutri/video4/my-app/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const HelloWorld = () => {
-  return <p>Hello World</p>
-}
+import HelloComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <StateFullComponent />
   </React.StrictMode>
 );
 
