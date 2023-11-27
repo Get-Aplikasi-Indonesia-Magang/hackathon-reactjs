92b692348d9eb2c4a6c2cf0b071a00d516437d06
asafik <ronismk7@gmail.com>
2023-11-27 22:32:31 +0700

    8 prat 2

---
+++ AsafikDaroini/video8part2/react-hello-word/src/container/Home.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Component } from 'react';
+
+// import YouTubeComp from '../components/YoutubeComp/YouTubeComp'
+// import Product from './Product/Product';
+import LifeCycleComp from './LifeCycleComp/LifeCycleComp';
+
+class Home extends Component {
+    state = {
+        showComponent:true 
+    }
+
+    componentDidMount() {
+    //    setTimeout(() => {
+    //     this.setState({
+    //         showComponent:false
+    //     })
+    //    }, 15000)
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <p>YouTube Components</p>
+                <hr />
+                 <YouTubeComp 
+                 time='7.12' 
+                 title='Video Monyet Pongo'
+                 desc='5x ditonton 2 harri yang lalu'/>
+                 
+                 <YouTubeComp 
+                 time ='8.02' 
+                 title='Video Monyet Pongo'
+                 desc='50x ditonton 10 harri yang lalu'/>
+                 <YouTubeComp 
+                 time ='5.04' 
+                 title='Video Monyet Pongo'
+                 desc='500x ditonton 20 harri yang lalu'/>
+                 <YouTubeComp 
+                 time='4.12' 
+                 title='Video Monyet Pongo'
+                 desc='700x ditonton 30 harri yang lalu'/>
+                 <YouTubeComp/>
+                 <p>Couter</p>
+                 <hr />
+                 <Product/> */}
+                 <p>Life Cycle Component</p>
+                 <hr />
+                 {
+                    this.state.showComponent 
+                    ?
+                    <LifeCycleComp/> 
+                    : null
+                 }
+                
+            </div>
+            
+        )
+    }
+}
+
+export default Home
