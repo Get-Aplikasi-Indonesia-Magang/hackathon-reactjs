d1777a68ca61c3c5803fce8845e6816b787a9980
Didin Nugroho <didinnugroho69@gmail.com>
2023-11-28 11:16:09 +0700

    Progres Video 1-9

---
+++ DidinDwiNugroho/video9/react-hello-world/src/component/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Post = (props) => {
+  return (
+    <div className="post">
+      <div className="img-thumb">
+        <img src="https://source.unsplash.com/200x150?" alt="dummy" />
+      </div>
+      <div className="content">
+        <p className="title">{props.data.title}</p>
+        <p className="desc">{props.data.body}</p>
+        <button className="remove" onClick={() => props.onRemove(props.data.id)}>Remove</button>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
