9d0eef5dab442f8d34c5c1d39a765a29392cf85a
Amalia Sakinah Putri <amalia2019@gmail.com>
2023-11-28 11:35:57 +0700

    update video7

--- AmaliaSakinahPutri/video4/my-app/src/container/Home/Home.jsx
+++ AmaliaSakinahPutri/video4/my-app/src/container/Home/Home.jsx
@@ -1,29 +1,32 @@
 import React, {Component} from "react";
 import YouTubeComp from "../../component/YouTubeComp/YouTubeComp";
-
+import Product from "../Product/Product";
 class Home extends Component {
     render() {
         return(
             <div>
-                <p>YouTube Component</p>
+                {/* <p>YouTube Component</p>
                 <hr/>
                 <YouTubeComp
-                 time="7.12"
-                 title="Film Barbie - Bagian 1"
-                 desc="2x ditonton, 2 hari yang lalu"/>
+                    time="7.12"
+                    title="Film Barbie - Bagian 1"
+                    desc="2x ditonton, 2 hari yang lalu"/>
                 <YouTubeComp 
-                 time="8.02" 
-                 title="Film Barbie - Bagian 2"
-                 desc="200x ditonton, 10 hari yang lalu"/>
+                    time="8.02" 
+                    title="Film Barbie - Bagian 2"
+                    desc="200x ditonton, 10 hari yang lalu"/>
                 <YouTubeComp 
-                 time="5.04" 
-                 title="Film Barbie - Bagian 3"
-                 desc="500x ditonton, 4 hari yang lalu"/>
+                    time="5.04" 
+                    title="Film Barbie - Bagian 3"
+                    desc="500x ditonton, 4 hari yang lalu"/>
                 <YouTubeComp 
-                 time="4.12" 
-                 title="Film Barbie - Bagian 4"
-                 desc="1k ditonton, 14 hari yang lalu"/>
-                <YouTubeComp/>
+                    time="4.12" 
+                    title="Film Barbie - Bagian 4"
+                    desc="1k ditonton, 14 hari yang lalu"/>
+                <YouTubeComp/> */}
+                <p>Counter</p>
+                <hr/>
+                <Product />
             </div>
             
         )
