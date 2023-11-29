9637b3a9ae937d297f006fab71537fc617ac16ea
Alvin Ramadhani <xbirucyberworldnet@gmail.com>
2023-11-29 23:37:33 +0700

    update

---
+++ Muhammad Alvio Dimas Hidayat/Video/video 6/anjay-mabar/src/container/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import YouTubeComponent from "../../component/YouTubeComponent/TouTubeComponent";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>Youtube Component</p>
+                <hr />
+                <YouTubeComponent 
+                    time="7.12" 
+                    title="Tutorial JS bagian - 1"
+                    desc="2x ditonton. 2 hari yang lalu" />
+                <YouTubeComponent 
+                    time="5.17" 
+                    title="Tutorial JS bagian - 2"
+                    desc="200x ditonton. 5 hari yang lalu" />
+                <YouTubeComponent 
+                    time="2.24" 
+                    title="Tutorial JS bagian - 3"
+                    desc="2155x ditonton. 3 hari yang lalu" />
+                <YouTubeComponent 
+                    time="7.48" 
+                    title="Tutorial JS bagian - 4"
+                    desc="22x ditonton. 5 hari yang lalu" />
+                <YouTubeComponent />
+            </div>
+        );
+    }
+}
+
+export default Home;
