4d8eb04f31a0330d1e257746ef07b696957f32d4
bagusdaffaf01@gmail.com <bagusdaffaf01@gmail.com>
2023-11-30 22:34:03 +0700

    update new

--- BagusDaffaFirmansyah/Ide/react-question/src/index.js
+++ BagusDaffaFirmansyah/Ide/react-question/src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import firebase from './firebase';
-
-console.log('config firebase ===>', firebase);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
